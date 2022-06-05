a75d48b245f0d69d78506e81d0d7ea009ef887f2
TildaDares <mathildaudufo@gmail.com>
2022-06-05 16:20:17 +0100

    add career advice section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Loader from "./components/Loader";
+import CareerAdvice from "./components/CareerAdvice";
 
 function App() {
   const [internshipData, setInternshipData] = useState([]);
@@ -9,13 +11,12 @@ function App() {
 
   useEffect(() => {
     loaderRef.current.style.display = "flex";
-    fetch("https://internships-web-scraper.herokuapp.com/results")
+    axios
+      .get("https://internships-web-scraper.herokuapp.com/results")
       .then((response) => {
-        console.log(response);
-        return response.json();
+        return response.data;
       })
       .then((data) => {
-        console.log(data);
         setInternshipData(data);
         loaderRef.current.style.display = "none";
       })
@@ -25,34 +26,37 @@ function App() {
   return (
     <>
       <Navbar />
-      <Loader loaderRef={loaderRef} />
-      <section className="internships-section">
-        {internshipData.map((internship, index) => (
-          <>
-            <div key={index} className="internship">
-              <img
-                src={internship.logo}
-                alt={internship.company + " logo"}
-                className="logo"
-              />
-              <div className="internship-detail">
-                <a
-                  href={internship.link}
-                  className="title"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {internship.title}
-                </a>
-                <p className="company">{internship.company}</p>
-                <p className="location">{internship.location}</p>
-                <small>{internship.datePosted}</small>
+      <main>
+        <section className="internships-section">
+          <Loader loaderRef={loaderRef} />
+          {internshipData.map((internship, index) => (
+            <>
+              <div key={index} className="internship">
+                <img
+                  src={internship.logo}
+                  alt={internship.company + " logo"}
+                  className="logo"
+                />
+                <div className="internship-detail">
+                  <a
+                    href={internship.link}
+                    className="title"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {internship.title}
+                  </a>
+                  <p className="company">{internship.company}</p>
+                  <p className="location">{internship.location}</p>
+                  <small>{internship.datePosted}</small>
+                </div>
               </div>
-            </div>
-            <hr />
-          </>
-        ))}
-      </section>
+              <hr />
+            </>
+          ))}
+        </section>
+        <CareerAdvice />
+      </main>
     </>
   );
 }
---
+++ src/components/CareerAdvice.jsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+const CareerAdvice = () => {
+  const [careerAdvice, setCareerAdvice] = useState([]);
+
+  const options = {
+    method: "GET",
+    params: {
+      q: "resume job themuse.com interview",
+      pageNumber: "1",
+      pageSize: "10",
+      autoCorrect: "true",
+      safeSearch: "true",
+    },
+    headers: {
+      "X-RapidAPI-Host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+      "X-RapidAPI-Key": "f4b65f8b71mshd94f1c8f39c24c8p1db2e3jsn34588fe4b357",
+    },
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI?'",
+        options
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        setCareerAdvice(data.value);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <aside>
+      <h2>Career Advice</h2>
+      <ul>
+        {careerAdvice.map((resource, index) => (
+          <li key={index}>
+            <a href={resource.url}>{resource.title}</a>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default CareerAdvice;
