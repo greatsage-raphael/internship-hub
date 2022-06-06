b86d26010107805cab62dde9ad05ca6924e41563
Lubwama Collins <56563537+greatsage-raphael@users.noreply.github.com>
2022-06-06 21:52:55 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://internshiphub.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
